2ed636849babe5c980173ae11608515ba45ed9a5
Erin Bleiweiss <erinbleiweiss@gmail.com>
2016-05-10 20:27:47 -0500

    basic math operations

---
+++ mini-swift.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from yacc import yacc
+import swift
+
+
+with open('testfile.c', 'r') as content_file:
+    chunk = ""
+    for line in content_file:
+        print("")
+        result = yacc.parse(line)
+        if result is None:
+            chunk += line
+            if len(chunk) > 0:
+                print("chunk: {}".format(chunk))
+                chunk_res = yacc.parse(chunk)
+                if chunk_res is not None:
+                    print("AST is {}".format(chunk_res))
+                    r = swift.eval(chunk_res)
+                    if r is not None:
+                        print r
+                        chunk = ""
+        else:
+            print("AST is {}".format(result))
+            r = swift.eval(result)
+            if r is not None:
+                print r
---
+++ yacc.py
@@ -0,0 +1,81 @@
+import ply.yacc as yacc
+
+# Get the token map from the lexer.  This is required.
+from lex import tokens
+
+DEBUG = False
+
+# Namespace & built-in functions
+
+name = {}
+
+class DivideByZeroError(Exception):
+    def __init___(self):
+        Exception.__init__(self,"Divide by zero error")
+
+# BNF
+
+def p_program(p):
+    '''program : expressions'''
+    print "Saw: ", p[1]
+    ast = []
+    ast += p[1]
+    p[0] = ast
+
+def p_expressions(p):
+    '''expressions : expression
+                 | expressions expression'''
+    p[0] = p[1]
+
+def p_expression(p):
+    '''expression : operation'''
+    p[0] = p[1]
+
+def p_operation(p):
+    '''operation : addition
+                 | subtraction
+                 | multiplication
+                 | division'''
+    p[0] = p[1]
+
+def p_addition(p):
+    '''addition : INTEGER PLUS INTEGER
+                | INTEGER PLUS addition'''
+    p[0] = [p[2]] + [p[1], p[3]]
+
+def p_subtraction(p):
+    '''subtraction : INTEGER MINUS INTEGER
+                   | INTEGER MINUS subtraction'''
+    p[0] = [p[2]] + [p[1], p[3]]
+
+def p_multiplication(p):
+    '''multiplication : INTEGER MULT INTEGER
+                   | INTEGER MULT multiplication'''
+    p[0] = [p[2]] + [p[1], p[3]]
+
+def p_division(p):
+    '''division : INTEGER DIV INTEGER
+                   | INTEGER DIV division'''
+    if p[3] == 0:
+        raise DivideByZeroError
+    else:
+        p[0] = [p[2]] + [p[1], p[3]]
+
+def p_nil(p):
+    'nil : NIL'
+    p[0] = None
+
+def p_empty(t):
+    'empty : '
+    pass
+
+# Error rule for syntax errors
+def p_error(p):
+    print "Syntax error!! ",p
+
+# Build the parser
+# Use this if you want to build the parser using SLR instead of LALR
+# yacc.yacc(method="SLR")
+yacc.yacc()
+
+
